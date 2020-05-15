bceb18a27f6418b0d679592c201832b79bfd0ba4
Afsoon <said.atrahouch@gmail.com>
2020-05-15 11:34:21 +0200

    stdin input

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -4,8 +4,19 @@ extern crate clap;
 use regex::Regex;
 use clap::{App,Arg};
 use std::fs::File;
+use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
+    }
+}
 
 fn main() {
     let args = App::new("grep-lite")
@@ -18,21 +29,21 @@ fn main() {
         .arg(Arg::with_name("input")
             .help("File to search")
             .takes_value(true)
-            .required(true))
+            .required(false))
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let input = args.value_of("pattern").unwrap();
-    let file = File::open(input).unwrap();
-    let reader = BufReader::new(file);
+    let input = args.value_of("input").unwrap_or("-");
 
-    for line_ in reader.lines() {
-        let line = line_.unwrap();
-        match re.find(&line) { // line es un String entonces toma como argumento &str
-            Some(_) => println!("{}", line),
-            None => (), //() es un placeholder de null
-        }
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re)
+    } else {
+        let file = File::open(input).unwrap();
+        let reader = BufReader::new(file);
+        process_lines(reader, re)
     }
 }
